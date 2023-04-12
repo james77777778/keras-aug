edabc4084bc68d8ce27bf362a8fc99bba28d6ad3
chiuhongyu <20734616+james77777778@users.noreply.github.com>
2023-04-12 20:43:16 +0800

    Update .devcontainer

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,5 @@
+sudo pip install --upgrade pip
+sudo pip install git+https://github.com/keras-team/keras-cv.git tensorflow --upgrade
+sudo pip install matplotlib flake8 isort black
+sudo apt update
+sudo apt install -y clang-format
