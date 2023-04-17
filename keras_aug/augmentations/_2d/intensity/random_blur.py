9d0cf919ce21e9aae8d0f2b164cf35f469375d1d
chiuhongyu <20734616+james77777778@users.noreply.github.com>
2023-04-17 22:07:15 +0800

    Add RandomJpegQuality

--- keras_aug/augmentations/_2d/intensity/random_blur.py
+++ keras_aug/augmentations/_2d/intensity/random_blur.py
@@ -65,10 +65,9 @@ class RandomBlur(VectorizedBaseImageAugmentationLayer):
             "images": images,
             "blur_kernel_sizes": transformations,
         }
-        blurred_images = tf.map_fn(
+        blurred_images = tf.vectorized_map(
             self.blur_single_image,
             inputs_for_blur_single_image,
-            fn_output_signature=tf.float32,
         )
         return tf.cast(blurred_images, self.compute_dtype)
 
