a689a6ed78ed1ce9cb36ad5650f92478e6d99592
HongYu <20734616+james77777778@users.noreply.github.com>
2023-04-21 10:00:26 +0000

    Add dist scripts

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -3,5 +3,6 @@ sudo pip install git+https://github.com/keras-team/keras-cv.git tensorflow --upg
 sudo pip install matplotlib flake8 isort black pycocotools pytest pytest-cov
 sudo pip install --upgrade protobuf==3.20.1
 sudo pip install -r requirements/docs.txt
+sudo pip install -r requirements/dist.txt
 sudo apt update
 sudo apt install -y clang-format
---
+++ setup.py
@@ -0,0 +1,63 @@
+"""Setup script. Copied and modified from
+https://github.com/keras-team/keras-cv/blob/master/setup.py
+"""
+
+import pathlib
+
+from setuptools import find_packages
+from setuptools import setup
+from setuptools.dist import Distribution
+
+BUILD_WITH_CUSTOM_OPS = False
+
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
+
+
+class BinaryDistribution(Distribution):
+    """This class is needed in order to create OS specific wheels."""
+
+    def has_ext_modules(self):
+        return BUILD_WITH_CUSTOM_OPS
+
+    def is_pure(self):
+        return not BUILD_WITH_CUSTOM_OPS
+
+
+setup(
+    name="keras-aug",
+    description="Industry-strength augmentation extensions for KerasCV.",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/james77777778/keras-aug",
+    author="Hongyu, Chiu (james77777778)",
+    author_email="",
+    license="Apache License 2.0",
+    install_requires=["packaging", "absl-py", "regex", "tensorflow-datasets"],
+    python_requires=">=3.7",
+    extras_require={
+        "tests": [
+            "flake8",
+            "isort",
+            "black[jupyter]",
+            "pytest",
+            "pycocotools",
+            "tensorflow",
+            "keras-cv @ git+https://github.com/keras-team/keras-cv.git",
+        ],
+        "examples": ["tensorflow-datasets", "matplotlib"],
+    },
+    distclass=BinaryDistribution,
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.7",
+        "Operating System :: Unix",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: MacOS",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Software Development",
+    ],
+    packages=find_packages(exclude=("*_test.py",)),
+    include_package_data=True,
+)
