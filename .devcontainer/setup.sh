648a6266b7fe78d3fe30ff6a91ab31736d8a368e
HongYu <20734616+james77777778@users.noreply.github.com>
2023-04-28 05:56:24 +0000

    Update devcontainer

--- .devcontainer/setup.sh
+++
@@ -1,9 +0,0 @@
-sudo pip install --upgrade pip
-sudo pip install tensorflow-cpu==2.12.0
-sudo pip install git+https://github.com/keras-team/keras-cv.git
-sudo pip install matplotlib flake8 isort black pycocotools pytest pytest-cov
-sudo pip install --upgrade protobuf==3.20.1
-sudo pip install -r requirements/docs.txt
-sudo pip install -r requirements/dist.txt
-sudo apt update
-sudo apt install -y clang-format
