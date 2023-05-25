479e1b412b4e1b36e6ee3148277d78e7f7e56657
chiuhongyu <20734616+james77777778@users.noreply.github.com>
2023-05-25 22:56:35 +0800

    Release v0.5.2

--- keras_aug/__init__.py
+++ keras_aug/__init__.py
@@ -7,4 +7,4 @@ from keras_aug.core import SignedConstantFactorSampler
 from keras_aug.core import SignedNormalFactorSampler
 from keras_aug.core import UniformFactorSampler
 
-__version__ = "0.5.1"
+__version__ = "0.5.2"
