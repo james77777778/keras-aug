413528fea9d782968db6e0199cbd1bceb87f4df0
chiuhongyu <20734616+james77777778@users.noreply.github.com>
2023-05-30 22:46:01 +0800

    Move pytest and pytest-cov to pyproject.toml

--- keras_aug/conftest.py
+++ keras_aug/conftest.py
@@ -1,5 +1,7 @@
 import tensorflow as tf
 
-physical_devices = tf.config.list_physical_devices("GPU")
-for device in physical_devices:
-    tf.config.experimental.set_memory_growth(device, True)
+
+def pytest_configure(config):
+    physical_devices = tf.config.list_physical_devices("GPU")
+    for device in physical_devices:
+        tf.config.experimental.set_memory_growth(device, True)
