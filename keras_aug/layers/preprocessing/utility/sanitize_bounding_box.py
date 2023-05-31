d1f9961a4ac17b1f284742043ef715bdd3181626
HongYu <20734616+james77777778@users.noreply.github.com>
2023-05-31 07:03:52 +0000

    Allow dense bounding_boxes

--- keras_aug/layers/preprocessing/utility/sanitize_bounding_box.py
+++ keras_aug/layers/preprocessing/utility/sanitize_bounding_box.py
@@ -6,7 +6,6 @@ from keras_aug.layers.base.vectorized_base_random_layer import (
 )
 from keras_aug.utils import bounding_box as bounding_box_utils
 from keras_aug.utils.augmentation import BOUNDING_BOXES
-from keras_aug.utils.augmentation import IMAGES
 
 
 @keras.utils.register_keras_serializable(package="keras_aug")
@@ -24,20 +23,30 @@ class SanitizeBoundingBox(VectorizedBaseRandomLayer):
         - `torchvision <https://github.com/pytorch/vision>`_
     """
 
-    def __init__(self, min_size, bounding_box_format, **kwargs):
+    def __init__(self, min_size, bounding_box_format=None, **kwargs):
         super().__init__(**kwargs)
         self.min_size = min_size
         self.bounding_box_format = bounding_box_format
+
+    def augment_ragged_image(self, image, transformation, **kwargs):
+        return image
+
+    def augment_images(self, images, transformations, **kwargs):
+        return images
+
+    def augment_labels(self, labels, transformations, **kwargs):
+        return labels
+
+    def augment_bounding_boxes(
+        self, bounding_boxes, transformations, images=None, **kwargs
+    ):
         if self.bounding_box_format is None:
             raise ValueError(
+                "`SanitizeBoundingBox()` was called with bounding boxes,"
+                "but no `bounding_box_format` was specified in the constructor."
                 "Please specify a bounding box format in the constructor. i.e."
-                "`SanitizeBoundingBox(bounding_box_format='xyxy')`"
+                "`SanitizeBoundingBox(..., bounding_box_format='xyxy')`"
             )
-
-    def _batch_augment(self, inputs):
-        self._validate_inputs(inputs)
-        images = inputs.get(IMAGES, None)
-        bounding_boxes = inputs.get(BOUNDING_BOXES, None)
         bounding_boxes = bounding_box.to_dense(bounding_boxes)
         bounding_boxes = bounding_box_utils.sanitize_bounding_boxes(
             bounding_boxes,
@@ -45,15 +54,19 @@ class SanitizeBoundingBox(VectorizedBaseRandomLayer):
             bounding_box_format=self.bounding_box_format,
             images=images,
         )
-        bounding_boxes = bounding_box.to_ragged(
-            bounding_boxes, dtype=self.compute_dtype
-        )
-        result = {IMAGES: images, BOUNDING_BOXES: bounding_boxes}
+        return bounding_boxes
 
-        # preserve any additional inputs unmodified by this layer.
-        for key in inputs.keys() - result.keys():
-            result[key] = inputs[key]
-        return result
+    def augment_segmentation_masks(
+        self, segmentation_masks, transformations, **kwargs
+    ):
+        return segmentation_masks
+
+    def augment_keypoints(self, keypoints, transformations, **kwargs):
+        return keypoints
+
+    def _batch_augment(self, inputs):
+        self._validate_inputs(inputs)
+        return super()._batch_augment(inputs)
 
     def _validate_inputs(self, inputs):
         bounding_boxes = inputs.get(BOUNDING_BOXES, None)
