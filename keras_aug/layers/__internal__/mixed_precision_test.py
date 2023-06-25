Fix numerical issuse when using mixed_precision

keras_aug/layers/__internal__/mixed_precision_test.py
@@ -363,9 +363,10 @@ class MixedPrecisionTest(tf.test.TestCase, parameterized.TestCase):
 
     @parameterized.named_parameters(*GENERAL_TESTS)
     def test_run_in_mixed_precision(self, layer_cls, args, is_bbox_compatible):
+        tf.debugging.enable_check_numerics()
         keras.mixed_precision.set_global_policy("mixed_float16")
         images = tf.cast(
-            tf.random.uniform(shape=(2, 32, 32, 3)) * 255.0,
+            tf.random.uniform(shape=(2, 224, 224, 3)) * 255.0,
             dtype=tf.float64,
         )
         labels = tf.cast(
@@ -410,9 +411,10 @@ class MixedPrecisionTest(tf.test.TestCase, parameterized.TestCase):
     def test_run_in_mixed_precision_and_build_in_runtime(
         self, layer_cls, args, build_layer_cls, build_args
     ):
+        tf.debugging.enable_check_numerics()
         keras.mixed_precision.set_global_policy("mixed_float16")
         images = tf.cast(
-            tf.random.uniform(shape=(4, 32, 32, 3)) * 255.0,
+            tf.random.uniform(shape=(4, 224, 224, 3)) * 255.0,
             dtype=tf.float64,
         )
         labels = tf.cast(
@@ -441,3 +443,4 @@ class MixedPrecisionTest(tf.test.TestCase, parameterized.TestCase):
     def tearDownClass(cls) -> None:
         # Do not affect other tests
         keras.mixed_precision.set_global_policy("float32")
+        tf.debugging.disable_check_numerics()

keras_aug/layers/augmentation/intensity/random_clahe_test.py
@@ -79,7 +79,7 @@ class RandomCLAHETest(tf.test.TestCase):
 
         self.assertNotAllClose(image, output)
 
-    def test_RandomCLAHE_output(self):
+    def test_random_clahe_output(self):
         image_shape = (4, 8, 8, 3)
         image = tf.random.uniform(shape=image_shape) * 255.0
         image = tf.cast(image, dtype=tf.uint8)

keras_aug/layers/augmentation/intensity/random_hsv.py
@@ -154,14 +154,16 @@ class RandomHSV(VectorizedBaseRandomLayer):
         return keypoints
 
     def adjust_hue(self, images, transformations):
-        images = tf.image.rgb_to_hsv(images)
-        hue_factors = transformations["hue_factors"]
+        # cast to float32 to avoid numerical issue
+        images = tf.image.rgb_to_hsv(tf.cast(images, dtype=tf.float32))
+        hue_factors = tf.cast(transformations["hue_factors"], dtype=tf.float32)
         h_channels = tf.math.floormod(images[..., 0:1] + hue_factors, 1.0)
         images = tf.concat(
             [h_channels, images[..., 1:2], images[..., 2:3]], axis=-1
         )
         images = tf.image.hsv_to_rgb(images)
-        return images
+        images = tf.clip_by_value(images, 0, 255)
+        return tf.cast(images, dtype=self.compute_dtype)
 
     def adjust_saturation(self, images, transformations):
         saturation_factors = transformations["saturation_factors"]

keras_aug/layers/augmentation/mix/cut_mix.py
@@ -49,8 +49,8 @@ class CutMix(VectorizedBaseRandomLayer):
     def get_random_transformation_batch(
         self, batch_size, images=None, **kwargs
     ):
-        height = tf.cast(tf.shape(images)[H_AXIS], dtype=self.compute_dtype)
-        width = tf.cast(tf.shape(images)[W_AXIS], dtype=self.compute_dtype)
+        height = tf.cast(tf.shape(images)[H_AXIS], dtype=tf.float32)
+        width = tf.cast(tf.shape(images)[W_AXIS], dtype=tf.float32)
         permutation_order = self._random_generator.random_uniform(
             (batch_size,),
             minval=0,
@@ -63,20 +63,14 @@ class CutMix(VectorizedBaseRandomLayer):
             seed_beta=self._random_generator.make_seed_for_stateless_op(),
             alpha=self.alpha,
             beta=self.alpha,
-            dtype=self.compute_dtype,
+            dtype=tf.float32,
         )
         ratios = tf.math.sqrt(1.0 - lambda_samples)
-        height = tf.cast(tf.shape(images)[H_AXIS], dtype=self.compute_dtype)
-        width = tf.cast(tf.shape(images)[W_AXIS], dtype=self.compute_dtype)
         cut_heights = tf.cast(ratios * height, dtype=tf.int32)
         cut_widths = tf.cast(ratios * width, dtype=tf.int32)
-        center_xs = self._random_generator.random_uniform(
-            shape=(batch_size,), dtype=self.compute_dtype
-        )
+        center_xs = self._random_generator.random_uniform(shape=(batch_size,))
         center_xs = tf.cast(center_xs * width, dtype=tf.int32)
-        center_ys = self._random_generator.random_uniform(
-            shape=(batch_size,), dtype=self.compute_dtype
-        )
+        center_ys = self._random_generator.random_uniform(shape=(batch_size,))
         center_ys = tf.cast(center_ys * height, dtype=tf.int32)
         return {
             "permutation_order": permutation_order,