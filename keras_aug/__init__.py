4575301ac6ddbf6e28497676b5dedb0dc1b3868e
HongYu <20734616+james77777778@users.noreply.github.com>
2023-07-01 17:04:23 +0800

    Release v0.5.6 (#109)

--- keras_aug/__init__.py
+++ keras_aug/__init__.py
@@ -8,4 +8,4 @@ from keras_aug.core import SignedConstantFactorSampler
 from keras_aug.core import SignedNormalFactorSampler
 from keras_aug.core import UniformFactorSampler
 
-__version__ = "0.5.5"
+__version__ = "0.5.6"
